d6738d1fdcc7a1d7ed2471b94a7b7e7e4aef6b32
abdullah0306 <abdullahtariq4946125@gmail.com>
2025-05-10 18:57:51 +0500

    Solving build issues

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse } from 'next/server';
 import bcrypt from 'bcryptjs';
 import { SignJWT } from 'jose';
-import prisma from '../../../lib/prisma';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export async function POST(req: Request) {
   const JWT_SECRET = process.env.JWT_SECRET;
