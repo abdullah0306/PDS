3d4cb10a59f77aa2c435906c810b626f844ad1e2
abdullah0306 <abdullahtariq4946125@gmail.com>
2025-05-10 19:11:17 +0500

    Solving build issues

--- src/app/api/login/route.config.ts
+++ src/app/api/login/route.config.ts
@@ -1,1 +1,3 @@
 export const runtime = 'nodejs';
+export const preferredRegion = 'auto';
+export const dynamic = 'force-dynamic';
--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -3,7 +3,13 @@ import bcrypt from 'bcryptjs';
 import { SignJWT } from 'jose';
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 const getJWTSecret = () => {
   const secret = process.env.JWT_SECRET;
